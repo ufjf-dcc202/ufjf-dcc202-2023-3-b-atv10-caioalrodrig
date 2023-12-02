634cf55f3b29c0f85b2e76ff984baaa2b737c2b5
caioalrodrig <caio.almeida@engenharia.ufjf.br>
2023-12-02 11:36:27 -0300

    main.js ok

---
+++ main.js
@@ -0,0 +1,11 @@
+import { deJoaoParaMaria, getMaria, getJoao } from 'joaoEMaria.js';
+
+console.log("Modulo main");
+
+console.log("Joao tem", getJoao()," maçãs!")
+console.log("Maria tem", getMaria()," maçãs!")
+
+deJoaoParaMaria();
+
+console.log("Joao tem", getJoao()," maçãs!")
+console.log("Maria tem", getMaria()," maçãs!")
